4640505f3caceb16b3491e52592f46dddf664927
hpnightowl <iamhp2k@gmail.com>
2022-08-17 11:19:42 +0200

    sdm660-common: Drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -78,7 +78,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
